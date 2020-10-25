09e81f1b612550c9145960e8393c1c9990ec484f
Chamara <chamara@spritzer.app>
2020-10-26 02:46:22 +0530

    Updates

---
+++ precache-manifest.57ef0600021300d9038bb5510e4bb670.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c540eda06c0e8b13b5303f649b2a9ec",
+    "url": "/cars/index.html"
+  },
+  {
+    "revision": "171f17ad62a4c755f58c",
+    "url": "/cars/static/css/2.60eec0d8.chunk.css"
+  },
+  {
+    "revision": "a8b0b5011e3c1c2a00a9",
+    "url": "/cars/static/css/main.88366c26.chunk.css"
+  },
+  {
+    "revision": "171f17ad62a4c755f58c",
+    "url": "/cars/static/js/2.dd0c25b0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cars/static/js/2.dd0c25b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b0b5011e3c1c2a00a9",
+    "url": "/cars/static/js/main.82c87d1b.chunk.js"
+  },
+  {
+    "revision": "2c75bc748387627acace",
+    "url": "/cars/static/js/runtime-main.305352bc.js"
+  },
+  {
+    "revision": "1b8c4fa0c76cfaf9118a20efadfd4734",
+    "url": "/cars/static/media/1.1b8c4fa0.jpg"
+  },
+  {
+    "revision": "16ea56ef4cfe5ec81f5903d11bfc3703",
+    "url": "/cars/static/media/2.16ea56ef.jpg"
+  },
+  {
+    "revision": "b5df9df48fef58f877c31d3c6d438d82",
+    "url": "/cars/static/media/3.b5df9df4.jpg"
+  },
+  {
+    "revision": "aa2cd5bed231ac8111f7d1deceb1c808",
+    "url": "/cars/static/media/4.aa2cd5be.jpg"
+  },
+  {
+    "revision": "2af92bf9b92cfb82e20d04ae22fd5ce8",
+    "url": "/cars/static/media/pic.2af92bf9.png"
+  }
+]);
